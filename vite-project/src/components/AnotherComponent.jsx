4bf92d222bc850a249699e3971a2672150b8166f
Alexander <alexanderm.kodehode@gmail.com>
2025-09-09 12:19:04 +0200

    I did something(?!)

--- vite-project/src/components/AnotherComponent.jsx
+++ vite-project/src/components/AnotherComponent.jsx
@@ -1,8 +1,9 @@
-import { styles } from "../styles/AnotherContainer.module.css";
-export default function AnotherComponent() {
+import styles from "../styles/AnotherContainer.module.css";
+function AnotherComponent() {
   return (
     <div className={styles.container}>
       <h1 className={styles.header}>This is another component!</h1>
     </div>
   );
 }
+export default AnotherComponent;
